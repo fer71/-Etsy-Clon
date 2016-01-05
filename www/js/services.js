f14cc0c809b86dfc3dd7e2a75c3fec44576a2b74
Fernando Valdes <fernando@valdes.com.mx>
2016-01-05 02:59:12 -0600

    Retrieving Data from the DB - Firebase is working, we are getting the 4 objects

--- www/js/services.js
+++ www/js/services.js
@@ -55,6 +55,9 @@ angular.module('starter.services', [])
 
     var Products = {
 
+      all: function(){
+        return products;
+      },
       saveProduct: function(product, image){
         var newProduct = {
           name: product.name,
