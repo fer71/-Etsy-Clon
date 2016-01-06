f1c5095bedc6727b9b1060a958537b15baf40aef
Fernando Valdes <fernando@valdes.com.mx>
2016-01-05 23:12:35 -0600

    Refactoring Controllers and Services to be more organized

---
+++ www/js/controllers/chats.js
@@ -0,0 +1,23 @@
+'use stirct';
+
+app.controller('ChatsCtrl', function($scope, Chats, Products) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+
+    $scope.image = 'http://res.cloudinary.com/fvo2006/image/upload/v1451556219/camisa_ihx3hr.jpg';
+
+    $scope.button = function(product){
+      console.log('button was clicked', product, $scope.image);
+      Products.saveProduct(product, $scope.image);
+    }
+});
--- www/js/services/products.js
+++ www/js/services/products.js
@@ -1,8 +1,6 @@
-angular.module('starter.services', [])
+'use strict';
 
-
-
-  .factory('Products', function(FURL, $firebaseArray) {
+app.factory('Products', function(FURL, $firebaseArray) {
     var ref = new Firebase(FURL);
     var products = $firebaseArray(ref.child('products'));
 
@@ -30,6 +28,3 @@ angular.module('starter.services', [])
     return Products;
 
   });
-
-
-
