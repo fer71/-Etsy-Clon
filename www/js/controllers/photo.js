25fb7534f36a5f04b1717d43585c8f288e96d066
Fernando Valdes <fernando@valdes.com.mx>
2016-01-11 15:22:31 -0600

    we add style to tab photo html, style css, make icons bigger and centering text etc, also we add two new buttons with ng click in tab photo html and their functions on photo js

--- www/js/controllers/photo.js
+++ www/js/controllers/photo.js
@@ -1,5 +1,13 @@
 'use strict';
 
 app.controller('PhotoCtrl', function($scope) {
+
+  $scope.takePhoto = function() {
+    console.log('takePhoto was clicked');
+  }
   
+  $scope.choosePhoto = function() {
+    console.log('will link up to photolibrary');
+  }
+
 });
